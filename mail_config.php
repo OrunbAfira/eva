4a3d98dc3b60927af71def766d5809090e7a1a62
Orunbafira <brunoassisfaria@gmail.com>
2025-11-28 02:34:44 -0300

    6

--- mail_config.php
+++ mail_config.php
@@ -7,5 +7,5 @@ if (!defined('SMTP_PASSWORD'))    define('SMTP_PASSWORD', getenv('SMTP_PASSWORD'
 if (!defined('SMTP_ENCRYPTION'))  define('SMTP_ENCRYPTION', getenv('SMTP_ENCRYPTION') ?: '');
 if (!defined('SMTP_SEND_ONLY_IF_USER_EXISTS')) define('SMTP_SEND_ONLY_IF_USER_EXISTS', false);
 if (!defined('SMTP_FROM'))        define('SMTP_FROM', getenv('SMTP_FROM') ?: '');
-if (!defined('SMTP_FROM_NAME'))   define('SMTP_FROM_NAME', getenv('SMTP_FROM_NAME') ?: '');
+if (!defined('SMTP_FROM_NAME'))   define('SMTP_FROM_NAME', getenv('SMTP_FROM_NAME') ?: '');//
 if (!defined('SMTP_AUTH'))        define('SMTP_AUTH', filter_var(getenv('SMTP_AUTH') ?: false, FILTER_VALIDATE_BOOLEAN));
